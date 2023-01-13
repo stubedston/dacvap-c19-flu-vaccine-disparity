3994c20d332912c8e790c4f19636a40c59e0ea43
Will Midgley <w.h.midgley@swansea.ac.uk>
2023-01-13 14:14:28 +0000

    Weekly vaccs and venn plots

---
+++ pool_venn.R
@@ -0,0 +1,149 @@
+# ==========================================================================
+# Clear and load
+# ==========================================================================
+cat("Clear and load!\n")
+
+setwd("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Code")
+
+rm(list = ls())
+
+library(tidyverse)
+library(patchwork)
+library(grid)
+library(eulerr)
+library(lubridate)
+library(scales)
+
+# England
+t_venn_england <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/england/vacc_venn_diag_cohort.csv")
+# Wales
+t_venn_wales_main <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/export_19-12-2022_diagrams/exploratory_diagrams/t_ea_flu_c19.csv") %>% select(-p)
+t_venn_wales_preg <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/export_19-12-2022_diagrams/exploratory_diagrams_preg/t_ea_flu_c19_preg.csv") %>% select(-p)
+
+# I exported the wrong data, so I'm getting it from the report that was approved also :P
+
+t_venn_wales_preg <- tribble(
+  ~c19_vacc_complete_flg, ~flu_vacc_complete_flg, ~n,
+  TRUE,      TRUE,     4690,
+  FALSE,     TRUE,     9120,
+  TRUE,      FALSE,    3780,
+  FALSE,     FALSE,    11840
+)
+
+# ==========================================================================
+# Process England data
+# ==========================================================================
+cat("processing...\n")
+
+colnames(t_venn_england) <- c("c19_vacc_complete_flg", "flu_vacc_complete_flg", "n_england")
+
+t_venn_england <-
+t_venn_england %>% mutate(
+  c19_vacc_complete_flg = as.logical(c19_vacc_complete_flg),
+  flu_vacc_complete_flg = as.logical(flu_vacc_complete_flg),
+  n_england = gsub(",","", t_venn_england$n_england) %>% as.integer()
+)
+
+t_venn_england_main <- t_venn_england[2:5,]
+
+t_venn_england_preg <- t_venn_england[10:13,]
+
+# ==========================================================================
+# Pool England and Wales
+# ==========================================================================
+cat("pooling data...\n")
+
+t_venn_pooled_main <-
+full_join(t_venn_england_main, t_venn_wales_main) %>% mutate(
+	n = n+n_england
+  ) %>% select(-n_england)
+t_venn_pooled_main$p <- round(t_venn_pooled_main$n/sum(t_venn_pooled_main$n) * 100,1)
+
+t_venn_pooled_preg <-
+full_join(t_venn_england_preg, t_venn_wales_preg) %>% mutate(
+  n = n+n_england
+  ) %>% select(-n_england)
+t_venn_pooled_preg$p <- round(t_venn_pooled_preg$n/sum(t_venn_pooled_preg$n) * 100,1)
+
+# ==========================================================================
+# Turn tables into a df 
+# ==========================================================================
+
+# Main cohort
+
+l_venn_pooled_main <- list()
+
+for (i in 1:nrow(t_venn_pooled_main)) {
+    d_venn_pooled_main <- data.frame(
+        c19_flg = as.logical(t_venn_pooled_main$c19_vacc_complete_flg[i]),
+        flu_flg = as.logical(t_venn_pooled_main$flu_vacc_complete_flg[i])
+    )
+    l_venn_pooled_main[[i]] <- sample_n(d_venn_pooled_main, size = t_venn_pooled_main$n[i], replace = TRUE)
+}
+
+d_venn_pooled_main <-
+  bind_rows(l_venn_pooled_main) %>%
+    mutate(
+      neither = !c19_flg & !flu_flg
+    )
+
+# Pregnant cohort
+
+l_venn_pooled_preg <- list()
+
+for (i in 1:nrow(t_venn_pooled_preg)) {
+    d_venn_pooled_preg <- data.frame(
+        c19_flg = as.logical(t_venn_pooled_preg$c19_vacc_complete_flg[i]),
+        flu_flg = as.logical(t_venn_pooled_preg$flu_vacc_complete_flg[i])
+    )
+    l_venn_pooled_preg[[i]] <- sample_n(d_venn_pooled_preg, size = t_venn_pooled_preg$n[i], replace = TRUE)
+}
+
+d_venn_pooled_preg <-
+  bind_rows(l_venn_pooled_preg) %>%
+    mutate(
+      neither = !c19_flg & !flu_flg
+    )
+
+
+
+# ==========================================================================
+# Plot the venn diagram
+# ==========================================================================
+cat("Time to start plotting!\n")
+
+# Main cohort
+
+euler(d_venn_pooled_main) %>% plot(counts = TRUE, labels = c("COVID", "", "Neither"), main = "Main cohort vaccine uptake") # same process for pregnant cohort
+grid.text(expression(bold("Flu")), x=0.51, y=0.47) # flu label
+grid.text(paste0(t_venn_pooled_main$p[2], "%"), x=0.096, y=0.427) # covid only %         # These are changed to fit in the right place
+grid.text(paste0(t_venn_pooled_main$p[1], "%"), x=0.515, y=0.448) # flu & covid %
+grid.text(paste0(t_venn_pooled_main$p[3], "%"), x=0.738, y=0.46) # flu only %
+grid.text(paste0(t_venn_pooled_main$p[4], "%"), x=0.855, y=0.23) # neither %
+p_venn_pooled_main <- recordPlot()
+
+print(p_venn_pooled_main)
+
+# Pregnant cohort
+
+euler(d_venn_pooled_preg) %>% plot(counts = TRUE, labels = c("COVID", "Flu", "Neither"), main = "Pregnant cohort vaccine uptake")
+grid.text(paste0(t_venn_pooled_preg$p[2], "%"), x=0.085, y=0.46) # covid only %
+grid.text(paste0(t_venn_pooled_preg$p[1], "%"), x=0.26, y=0.465) # flu & covid %
+grid.text(paste0(t_venn_pooled_preg$p[3], "%"), x=0.47, y=0.46) # flu only %
+grid.text(paste0(t_venn_pooled_preg$p[4], "%"), x=0.785, y=0.453) # neither %
+p_venn_pooled_preg <- recordPlot()
+
+print(p_venn_pooled_preg)
+
+# ==========================================================================
+# Save plots
+# ==========================================================================
+cat("saving...\n")
+
+png("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/Plots/p_venn_pooled_main.png", width = 600, height = 600)
+p_venn_pooled_main
+dev.off()
+
+png("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/Plots/p_venn_pooled_preg.png", width = 600, height = 600)
+p_venn_pooled_preg
+dev.off()
---
+++ pool_weekly_vacc.R
@@ -0,0 +1,270 @@
+# ==========================================================================
+# Clear and load
+# ==========================================================================
+cat("Clear and load!\n")
+
+setwd("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Code")
+
+rm(list = ls())
+
+library(tidyverse)
+library(patchwork)
+library(grid)
+library(eulerr)
+library(lubridate)
+library(scales)
+
+# England
+d_weekly_vacc_england_main <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/england/vacc_uptake_full_cohort.csv")
+d_weekly_vacc_england_preg <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/england/vacc_uptake_preg_cohort.csv")
+# Wales
+d_weekly_vacc_wales_main_c19 <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/export_19-12-2022_diagrams/exploratory_diagrams/d_c19_vacc_week.csv")
+d_weekly_vacc_wales_main_flu <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/export_19-12-2022_diagrams/exploratory_diagrams/d_flu_vacc_week.csv")
+d_weekly_vacc_wales_preg_c19 <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/export_19-12-2022_diagrams/exploratory_diagrams_preg/d_c19_vacc_week_preg.csv")
+d_weekly_vacc_wales_preg_flu <- read.csv("C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/export_19-12-2022_diagrams/exploratory_diagrams_preg/d_flu_vacc_week_preg.csv")
+
+cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+cbPalette2 <- c("#0072B2", "#D55E00", "#CC79A7", "#E69F00", "#56B4E9", "#009E73", "#F0E442")
+
+
+# ==========================================================================
+# Process England data
+# ==========================================================================
+cat("processing...\n")
+
+process.england <- function(data) { # nolint
+data %>%
+	mutate(
+		vacc_seq =
+			case_when(
+				vacc_seq == "FirstVaccinationDoseDate" ~ "dose1",
+				vacc_seq == "SecondVaccinationDoseDate" ~ "dose2",
+				vacc_seq == "ThirdVaccinationDoseDate" ~ "dose3",
+				vacc_seq == "FluVaccinationDate202021" ~ "2020",
+				vacc_seq == "FluVaccinationDate202122" ~ "2021"
+			),
+		vacc_type = 
+			case_when(
+				vacc_type == "COVID-19" ~ "c19",
+				vacc_type == "Influenza" ~ "flu"
+				),
+		n_england = n,
+		vacc_date = ymd(vacc_date) + days(1)
+		) %>%
+		select(
+			vacc_seq,
+			vacc_date,
+			n_england,
+			vacc_type
+	)
+}
+
+d_weekly_vacc_england_main_c19 <- process.england(d_weekly_vacc_england_main) %>% filter(vacc_type == "c19")
+d_weekly_vacc_england_main_flu <- process.england(d_weekly_vacc_england_main) %>% filter(vacc_type == "flu")
+d_weekly_vacc_england_preg_c19 <- process.england(d_weekly_vacc_england_preg) %>% filter(vacc_type == "c19")
+d_weekly_vacc_england_preg_flu <- process.england(d_weekly_vacc_england_preg) %>% filter(vacc_type == "flu")
+
+# ==========================================================================
+# Process Wales data
+# ==========================================================================
+
+process.wales <- function(data) {
+data %>% mutate(
+	vacc_seq = case_when(
+		vacc_type == "flu" ~ paste0("20",vacc_seq),
+		TRUE ~ paste(vacc_seq)
+		),
+	vacc_date = ymd(vacc_date)
+	)
+}
+
+d_weekly_vacc_wales_main_c19 <- process.wales(d_weekly_vacc_wales_main_c19)
+d_weekly_vacc_wales_main_flu <- process.wales(d_weekly_vacc_wales_main_flu)
+d_weekly_vacc_wales_preg_c19 <- process.wales(d_weekly_vacc_wales_preg_c19)
+d_weekly_vacc_wales_preg_flu <- process.wales(d_weekly_vacc_wales_preg_flu)
+
+# ==========================================================================
+# Pool England and Wales
+# ==========================================================================
+cat("pooling data...\n")
+pool.data <- function(england, wales) {
+	full_join(england, wales) %>%
+	mutate(
+		n = replace_na(n, 0),
+		n_england = replace_na(n_england, 0),
+		n = n + n_england
+	) %>%
+	select(-n_england)
+}
+
+d_weekly_vacc_pooled_main_c19 <- pool.data(d_weekly_vacc_england_main_c19, d_weekly_vacc_wales_main_c19)
+d_weekly_vacc_pooled_main_flu <- pool.data(d_weekly_vacc_england_main_flu, d_weekly_vacc_wales_main_flu)
+d_weekly_vacc_pooled_preg_c19 <- pool.data(d_weekly_vacc_england_preg_c19, d_weekly_vacc_wales_preg_c19)
+d_weekly_vacc_pooled_preg_flu <- pool.data(d_weekly_vacc_england_preg_flu, d_weekly_vacc_wales_preg_flu)
+
+# ==========================================================================
+# Plotting graphs
+# ==========================================================================
+cat("Time to start plotting!\n")
+
+
+x_limits <- c(
+  ymd("2020-09-01"),
+  ymd("2022-03-31")
+)
+
+x_breaks <- seq(
+  from = ymd('2020-09-01'),
+  to   = ymd('2022-03-31'),
+  by   = "6 months"
+)
+
+y_limits <- c(0, 500000)
+y_limits_preg <- c(0, 23000) 
+
+
+# ==========================================================================
+# Main cohort
+# ==========================================================================
+
+p_c19_main <-
+  d_weekly_vacc_pooled_main_c19 %>%
+  ggplot(aes(
+    x = vacc_date,
+    y = n,
+    fill = vacc_seq
+  )) +
+  geom_col() +
+  scale_x_date(
+    name = "",
+    limits = x_limits,
+    breaks = x_breaks,
+    date_labels = "%b\n%Y"
+  ) +
+  scale_y_continuous(
+    name = "Number per week",
+    limits = y_limits,
+    breaks = breaks_pretty(),
+    labels = comma
+  ) +
+  labs(
+    fill = "Vaccine Dose"
+  ) +
+  scale_fill_manual(
+    values = cbPalette,
+    labels = c("Dose 1", "Dose 2", "Dose 3")
+  ) +
+  ggtitle(
+    "COVID-19 vaccinations"
+  )
+
+p_flu_main <-
+  d_weekly_vacc_pooled_main_flu %>%
+  ggplot(aes(
+    x = vacc_date,
+    y = n,
+    fill = vacc_seq
+  )) +
+  geom_col() +
+  scale_x_date(
+    name = "",
+    limits = x_limits,
+    breaks = x_breaks,
+    date_labels = "%b\n%Y"
+  ) +
+  scale_y_continuous(
+    name = "Number per week",
+    limits = y_limits,
+    breaks = breaks_pretty(),
+    labels = comma
+  ) +
+  labs(
+    fill = "Vaccine Dose"
+  ) +
+  scale_fill_manual(
+    values = cbPalette2,
+    labels = c("Winter 2020", "Winter 2021")
+  ) +
+  ggtitle(
+    "Influenza vaccinations"
+  )
+
+p_main <- p_c19_main / p_flu_main
+
+
+# ==========================================================================
+# Pregnant cohort
+# ==========================================================================
+
+
+p_c19_preg <-
+  d_weekly_vacc_pooled_preg_c19 %>%
+  ggplot(aes(
+    x = vacc_date,
+    y = n,
+    fill = vacc_seq
+  )) +
+  geom_col() +
+  scale_x_date(
+    name = "",
+    limits = x_limits,
+    breaks = x_breaks,
+    date_labels = "%b\n%Y"
+  ) +
+  scale_y_continuous(
+    name = "Number per week",
+    limits = y_limits_preg,
+    breaks = breaks_pretty(),
+    labels = comma
+  ) +
+  labs(
+    fill = "Vaccine Dose"
+  ) +
+  scale_fill_manual(
+    values = cbPalette,
+    labels = c("Dose 1", "Dose 2", "Dose 3")
+  ) +
+  ggtitle(
+    "COVID-19 vaccinations"
+  )
+
+p_flu_preg <-
+  d_weekly_vacc_pooled_preg_flu %>%
+  ggplot(aes(
+    x = vacc_date,
+    y = n,
+    fill = vacc_seq
+  )) +
+  geom_col() +
+  scale_x_date(
+    name = "",
+    limits = x_limits,
+    breaks = x_breaks,
+    date_labels = "%b\n%Y"
+  ) +
+  scale_y_continuous(
+    name = "Number per week",
+    limits = y_limits_preg,
+    breaks = breaks_pretty(),
+    labels = comma
+  ) +
+  labs(
+    fill = "Vaccine Dose"
+  ) +
+  scale_fill_manual(
+    values = cbPalette2,
+    labels = c("Winter 2020", "Winter 2021")
+  ) +
+  ggtitle(
+    "Influenza vaccinations"
+  )
+
+p_preg <- p_c19_preg / p_flu_preg
+
+# ==========================================================================
+# Save plots
+# ==========================================================================
+cat("saving...\n")
+
+ggsave("p_weekly_vacc_main.png", p_main, "png", "C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/Plots", width = 10, height = 10)
+ggsave("p_weekly_vacc_preg.png", p_preg, "png", "C:/Users/william.midgley/Documents/dcp02_covid_v_flu_coverage_disparities/Results/Plots", width = 10, height = 10)
